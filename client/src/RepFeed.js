90a34966ffed89b2e70d295a851c9c297e469fcd
Maxwell Nelson <maxwellnelson@Maxwells-MacBook-Pro.local>
2022-08-26 23:57:21 -0600

    replaced app.js with repfeed.js, removed create-react-app default jsx

---
+++ client/src/RepFeed.js
@@ -0,0 +1,44 @@
+import './RepFeed.css';
+
+const RepFeed = () => {
+
+
+    const reps = [
+        {
+            name: 'John-117',
+            state: 'Earth'
+        },
+        {
+            name: 'Cortana',
+            state: 'Earth'
+        }
+    ]
+
+    return (
+        <>
+            <div className='layout'>
+                <div className='layout-header'>Find your reps.</div>
+                <div className='layout-content'>
+                    {
+                        reps.map(rep => {
+                            return (
+                                <div className='rep'>
+                                    <div>
+                                        <span>Name:</span><span>{rep.name}</span>
+                                    </div>
+                                    <div>
+                                        <span>State:</span><span>{rep.state}</span>
+                                    </div>
+                                </div>
+
+                            )
+                        })
+                    }
+                </div>
+
+            </div>
+        </>
+    )
+};
+
+export default RepFeed;
